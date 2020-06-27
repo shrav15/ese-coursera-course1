376d71bfa51251f31477dab75921018ff2366308
shrav15 <67496971+shrav15@users.noreply.github.com>
2020-06-27 17:37:10 +0530

    Update stats.c

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -9,13 +9,16 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file Assin1 
+ * @brief c programming example 
  *
- * <Add Extended Description Here>
+ * this program analyzes an array of unsigned char data items and report
+    analytics on the maximum, minimum, mean, and median of the data set.
+    We also have reorder this data set from large to small. 
+    All statistics should be rounded down to the nearest integer.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author shravani patel
+ * @date 27-06-20
  *
  */
 
@@ -36,8 +39,135 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+  float mean =find_mean( unsigned char test,SIZE);
+  float meadian=find_median(unsigned char test,SIZE);
+  unsigned char maximum=find_maximum(unsigned char test,SIZE);
+  unsigned char minimum=find_minimum(unsigned char test,SIZE);
+  
   /* Statistics and Printing Functions Go Here */
-
+  
+ 
+void  print_statistics(float meadian,float mean,unsigned char maximum,unsigned char minimum) 
+{
+  
+      printf("the means is %f",mean);
+      printf("the median is %f",meadian);
+      printf("the maximum value is %c",maximum);
+      printf("the minimum value is %c",minimum);
+}
+void print_array(unsigned char *ptr,unsigned int size)
+{
+      for (int i=0;i<size;i++)
+      {
+        printf(%d \t", ptr[i]);
+        
+      }
+      
 }
 
-/* Add other Implementation File Code Here */
+float find_mean(unsigned char *ptr,unsigned int size)   //mean       
+               {
+                   unsigned int sum=0;
+                   for (int i=0;i<size;i++)
+            {
+                          sum=sum+ptr[i];
+                     
+                     
+          }         
+                 
+                    unsigned int nomean;
+                    nomean=(sum)/(size);
+                    return mean;
+               }
+           
+unsigned char find_maximum(unsigned char *ptr,unsigned int size)  //maximum
+
+   {          
+               unsigned int max=ptr[0];
+               for (int i=1;i<=size;i++)
+         {
+                      if (ptr[i]>max)
+                {           
+                        max=ptr[i];
+                        
+                }
+                 
+         }
+               
+               return max;
+     
+   }
+unsigned char find_minimum(unsigned char *ptr,unsigned int size)  //maximum
+
+   {          
+               unsigned int min=ptr[0];
+               for (int i=1;i<=size;i++)
+         {
+                      if (ptr[i]<min)
+                {           
+                        min=ptr[i];
+                        
+                }
+                 
+         }
+               
+               return min;
+     
+   }
+float find_median(unsigned char *ptr,unsigned int size)
+   {         
+     
+             unsigned char temp(SIZE) ;
+             for (int i=0;i<size;i++)
+             {
+               
+                 for (int j=i+1;j<size;j++)
+              {
+                     if (ptr[i]<ptr[j])
+                   {   
+                        temp=ptr[i];
+                        ptr[i]=ptr[j];
+                        ptr[j]=temp;
+         
+                   }
+                   
+             }
+               
+           }
+             unsigned int meadian;
+             if (size % 2==0)//when size is even 
+             {
+               unsigned int x=size/2;
+               unsigned int y=x+1;
+               meadian = (ptr[x]+ptr[y])/2;
+
+             }
+             else //when size is odd
+             {
+
+                 unsigned int z=(size+1)/2;
+                 meadian = ptr[z];
+
+
+             } 
+             return meadian;
+
+     }
+void sort_array(unsigned char *ptr,unsigned int size)
+ {    unsigned char = temp;
+      for (int i=0;i<size;i++)
+      {
+        for (int j=i+1;j<size;j++)
+        {
+             if (ptr[i]<ptr[j])
+             {   
+                   temp=ptr[i];
+                   ptr[i]=ptr[j];
+                   ptr[j]=temp;
+             }
+        }
+      }
+   }
+
+
+/* Add other Implementation File Code Her}e */
